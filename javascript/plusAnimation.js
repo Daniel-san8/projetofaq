0861ed7e492070d9b5f44eff68a1693fd234f968
Daniel San <danielgdt88@hotmail.com>
2024-05-30 22:48:37 -0300

    organização de pastas

--- javascript/plusAnimation.js
+++ javascript/plusAnimation.js
@@ -1,15 +1,11 @@
 export default function plusAnimation() {
   addEventListener("load", () => {
     const plus = document.querySelectorAll("[data-plus]");
-    const questions = document.querySelectorAll("[data-questions]");
     const dataPara = document.querySelectorAll("[data-para]");
     dataPara.forEach(function (itemPara) {
       itemPara.addEventListener("click", function () {
         const checked = this.nextElementSibling;
-        if (
-          checked.getAttribute("src") ===
-          "/faq-accordion-main/assets/images/icon-minus.svg"
-        ) {
+        if (checked.getAttribute("src") === "images/icon-minus.svg") {
         }
       });
     });
@@ -17,20 +13,11 @@ export default function plusAnimation() {
     //Função de click e ativação do minus
     plus.forEach(function (plusItem) {
       plusItem.addEventListener("click", () => {
-        if (
-          plusItem.getAttribute("src") ===
-          "/faq-accordion-main/assets/images/icon-plus.svg"
-        ) {
-          plusItem.setAttribute(
-            "src",
-            "/faq-accordion-main/assets/images/icon-minus.svg"
-          );
+        if (plusItem.getAttribute("src") === "images/icon-plus.svg") {
+          plusItem.setAttribute("src", "images/icon-minus.svg");
           plusItem.nextElementSibling.style.display = "flex";
         } else {
-          plusItem.setAttribute(
-            "src",
-            "/faq-accordion-main/assets/images/icon-plus.svg"
-          );
+          plusItem.setAttribute("src", "images/icon-plus.svg");
           plusItem.nextElementSibling.style.display = "none";
         }
       });
