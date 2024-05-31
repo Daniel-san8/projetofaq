9a2fab819d4d59f75d640dec7dcd357a2420fae9
Daniel San <danielgdt88@hotmail.com>
2024-05-30 23:19:41 -0300

    últimas alterações no responsivo

--- plusAnimation.js
+++ plusAnimation.js
@@ -1,23 +1,15 @@
 export default function plusAnimation() {
   addEventListener("load", () => {
     const plus = document.querySelectorAll("[data-plus]");
-    const dataPara = document.querySelectorAll("[data-para]");
-    dataPara.forEach(function (itemPara) {
-      itemPara.addEventListener("click", function () {
-        const checked = this.nextElementSibling;
-        if (checked.getAttribute("src") === "images/icon-minus.svg") {
-        }
-      });
-    });
 
     //Função de click e ativação do minus
     plus.forEach(function (plusItem) {
       plusItem.addEventListener("click", () => {
-        if (plusItem.getAttribute("src") === "images/icon-plus.svg") {
-          plusItem.setAttribute("src", "images/icon-minus.svg");
+        if (plusItem.getAttribute("src") === "/projeto/images/icon-plus.svg") {
+          plusItem.setAttribute("src", "/projeto/images/icon-minus.svg");
           plusItem.nextElementSibling.style.display = "flex";
         } else {
-          plusItem.setAttribute("src", "images/icon-plus.svg");
+          plusItem.setAttribute("src", "/projeto/images/icon-plus.svg");
           plusItem.nextElementSibling.style.display = "none";
         }
       });
