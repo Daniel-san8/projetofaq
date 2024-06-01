7397f74fadee18c146e6deb721a2d446484951ee
Daniel San <danielgdt88@hotmail.com>
2024-06-01 16:13:24 -0300

    touchstart e question color

--- plusAnimation.js
+++ plusAnimation.js
@@ -10,9 +10,11 @@ export default function plusAnimation() {
         if (img.getAttribute("src") === "images/icon-plus.svg") {
           img.setAttribute("src", "images/icon-minus.svg");
           resposta.style.display = "flex";
+          paragrafo.style.color = "#ad28eb";
         } else {
           img.setAttribute("src", "images/icon-plus.svg");
           resposta.style.display = "none";
+          paragrafo.style.color = "black";
         }
       });
     });
@@ -23,9 +25,42 @@ export default function plusAnimation() {
         if (plusItem.getAttribute("src") === "images/icon-plus.svg") {
           plusItem.setAttribute("src", "images/icon-minus.svg");
           plusItem.nextElementSibling.style.display = "flex";
+          plusItem.style.color = "#ad28eb";
         } else {
           plusItem.setAttribute("src", "images/icon-plus.svg");
           plusItem.nextElementSibling.style.display = "none";
+          plusItem.style.color = "black";
+        }
+      });
+    });
+
+    paragrafos.forEach((paragrafo) => {
+      paragrafo.addEventListener("touchstart", function () {
+        const img = this.nextElementSibling;
+        const resposta = this.nextElementSibling.nextElementSibling;
+        if (img.getAttribute("src") === "images/icon-plus.svg") {
+          img.setAttribute("src", "images/icon-minus.svg");
+          resposta.style.display = "flex";
+          paragrafo.style.color = "#ad28eb";
+        } else {
+          img.setAttribute("src", "images/icon-plus.svg");
+          resposta.style.display = "none";
+          paragrafo.style.color = "black";
+        }
+      });
+    });
+
+    //Função de click e ativação do minus
+    plus.forEach(function (plusItem) {
+      plusItem.addEventListener("touchstart", () => {
+        if (plusItem.getAttribute("src") === "images/icon-plus.svg") {
+          plusItem.setAttribute("src", "images/icon-minus.svg");
+          plusItem.nextElementSibling.style.display = "flex";
+          plusItem.style.color = "#ad28eb";
+        } else {
+          plusItem.setAttribute("src", "images/icon-plus.svg");
+          plusItem.nextElementSibling.style.display = "none";
+          plusItem.style.color = "black";
         }
       });
     });
